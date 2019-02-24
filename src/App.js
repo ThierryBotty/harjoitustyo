faf88a848250bf1cf8b2ff8cc61df5d34399cf9d
Thierry Botty <thierry.botty@helsinki.fi>
2019-02-24 22:37:16 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import xml2js from 'xml2js'
+import XML2JS from 'xml2js'
 
 const City = ({ city }) => {
   const [ weather, setWeather ] = useState(null)
-
-useEffect(() => {
-  axios.get(`http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&
-  storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&
-  place=${city}`)
-  .then(result => {
-    console.log(result.data.current)
-    setWeather(result.data.current)
+  if (city === "") {city = "helsinki"}
+  useEffect(() => {
+    axios.get(`http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&
+storedquery_id=fmi::forecast::hirlam::surface::point::multipointcoverage&
+place=${city}&`)
+    .then(result => {
+      XML2JS.parseString(result.data, (error, res) => {
+        console.log(res)
+        setWeather(res)
+      })
     })
   }, [])
 
-  return <div> <Weather weather = {weather}/></div>)
+return (
+  <>
+  <h2> Weather in {city} </h2>
+  <div> <Weather weather = {weather} city = {city}/> </div> </> )
 }
 
+const Weather = ( {weather, city} ) => {
+  if(!weather) return <div>Weather not found.</div>
 
-const Weather = ( {weather} ) => {
-  if(!weather) {return null}
+  let a = weather["wfs:FeatureCollection"]["wfs:member"][0]["omso:GridSeriesObservation"][0]["om:result"][0]["gmlcov:MultiPointCoverage"][0]["gmlcov:rangeType"][0]["swe:DataRecord"][0]["swe:field"]
+  let b = a[1]["$"]
+  let c = weather["wfs:FeatureCollection"]["wfs:member"][0]["omso:GridSeriesObservation"][0]["om:observedProperty"][0]["$"]
+  console.log(c)
   return(
     <>
       <div>
-      Temperature: {weather.temp_c} Celcius
+      Weekly forecast in {city}:
       </div>
       <div>
-      <b>Wind: </b>{weather.wind_kph} kph, direction {weather.wind_dir}
+      <b>Temperature: </b>
+      <svg> <use href={c["xlink:href"]} />
+      </svg>
     </div>
   </>)
 }
 
 const App = () => {
+  const [ city, setCity ] = useState('')
     return (
       <>
       <div>
         <form>
-          <label> City: <input type="text" name="city" /> </label>
+          <label> City: <input value={city} onChange={(x)=>{
+            x.preventDefault()
+
+            setCity(x.target.value)
+            }
+          } name="city" /> </label>
           <input type="submit" value="Submit" />
+          <City city = {city}/>
         </form>
       </div>
-
       </>
     )
   }
-
 
 export default App;
